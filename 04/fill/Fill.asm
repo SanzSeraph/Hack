e0b2386c97459ed099075a9853f023ce8a18d897
Daniel Arant <snoop.daniels@gmail.com>
2020-04-16 10:09:46 -0400

    Completed chapter 4 and started on chapter 5

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -1,8 +1,3 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
@@ -11,4 +6,50 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+// @KBD
+// D=M // Load the keyboard input into D
+@BLACK
+// D;JGT
+0;JMP
+@WHITE
+D;JEQ
+@LOOP
+0;JMP
+
+(BLACK)
+@color
+M=0;
+M=!M;
+@FILL
+0;JMP
+
+(WHITE)
+@color
+M=0;
+@FILL
+0;JMP
+
+(FILL)
+@SCREEN
+D=A
+@current
+M=D
+  (FILL_LOOP)
+  @current
+  D=M
+  @32767
+  D=A-D
+  @LOOP
+  D;JEQ // Once the offset has reached the end of the screen, break out of the loop
+  @color
+  D=M // store the "color" in D
+  @current
+  A=M // Set the address to the current address
+  M=D // Set the current chunk pointed to by A to the color stored in D
+  @current
+  M=M+1 // Increment the current screen address
+  @FILL_LOOP
+  0;JMP
+  
+ 
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -1,9 +1,30 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Initialize
+@R0
+D=M // Temporarily load first operand into D
+@i
+M=D // Set i to the first operand
+@product
+M=0
+
+(LOOP) 
+@i
+D=M // Load i into D so we can compare and conditionally jump
+@BREAK
+D;JEQ // if i = 0, leave the loop
+@R1
+D=M // Load the second operand into so we can sum with running product
+@product
+M=M+D
+@i
+M=M-1
+@LOOP
+0;JMP
+
+(BREAK)
+@product
+D=M
+@R2
+M=D // load answer into R2
